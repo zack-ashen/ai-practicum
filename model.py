f2aeb30b723daec66209bebee73c1711ada59f5d
Zack Ashen <zachary.h.a@gmail.com>
2023-10-30 10:47:52 -0400

    boilerplate

--- model.py
+++ model.py
@@ -1,64 +1,17 @@
-import os
+import openai
 
+# Initialize API client
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-##################################################################################################
-# In this section, we set the user authentication, user and app ID, model details, and the URL
-# of the image we want as an input. Change these strings to run your own example.
-#################################################################################################
+# Prepare your image
+with open("path_to_your_image.jpg", "rb") as f:
+    image = f.read()
 
-# Your PAT (Personal Access Token) can be found in the portal under Authentification
-PAT = os.environ.get('PAT')
-# Specify the correct user_id/app_id pairings
-# Since you're making inferences outside your app's scope
-USER_ID = 'clarifai'
-APP_ID = 'main'
-# Change these to whatever model and image URL you want to use
-MODEL_ID = 'logos-yolov5'
-MODEL_VERSION_ID = 'd4e85f4d37d24fdd9a1f04cca91a3510'
-IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg'
-
-############################################################################
-# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-############################################################################
-
-from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
-from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_code_pb2
-
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
-
-metadata = (('authorization', 'Key ' + PAT),)
-
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
-
-post_model_outputs_response = stub.PostModelOutputs(
-    service_pb2.PostModelOutputsRequest(
-        user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
-        model_id=MODEL_ID,
-        version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    image=resources_pb2.Image(
-                        url=IMAGE_URL
-                    )
-                )
-            )
-        ]
-    ),
-    metadata=metadata
+# Use the API to classify or describe the image
+response = openai.Image.create(
+    model="image-model-id",  # Replace with the appropriate model ID for image classification
+    data=image
 )
-if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
-    print(post_model_outputs_response.status)
-    raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
 
-# Since we have one input, one output will exist here
-output = post_model_outputs_response.outputs[0]
-
-print("Predicted concepts:")
-for concept in output.data.concepts:
-    print("%s %.2f" % (concept.name, concept.value))
-
-# Uncomment this line to print the full Response JSON
-#print(output)
+# Print the response or description
+print(response['data']['text'])
