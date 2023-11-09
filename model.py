d6e1ade95304e0cab83a44ac6e8ae103e8aa346a
Zack Ashen <zachary.h.a@gmail.com>
2023-11-08 19:44:29 -0500

    working...ish

--- model.py
+++ model.py
@@ -1,21 +1,50 @@
-import openai
+import base64
+import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
 # Initialize API client
-openai.api_key = os.getenv("OPEN_API_KEY")
+api_key = os.getenv("OPEN_API_KEY")
 
-# Prepare your image
-with open("path_to_your_image.jpg", "rb") as f:
-    image = f.read()
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Use the API to classify or describe the image
-response = openai.Image.create(
-    model="image-model-id",  # Replace with the appropriate model ID for image classification
-    data=image
-)
+# Path to your image
+image_path = "drinks2.png"
 
-# Print the response or description
-print(response['data']['text'])
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "What’s in this image?"
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/png;base64,{base64_image}"
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
